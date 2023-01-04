d5f17249e523cf12be66b68fc4e0f3949e849b44
devenperez <63876261+devenperez@users.noreply.github.com>
2023-01-04 15:22:32 -0500

    Added buttons to update problem counters
    Update button pulls the completed numbers from the active Leetcode account

--- popupButtons.js
+++ popupButtons.js
@@ -75,12 +75,12 @@ const DOWNLOADER_FUNC = `function(filename, text) {
 }`
 
 
-// Activates create README.md button
+// Activates buttons on correct sites
 chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     const LEETCODE_PROBLEM_URL = "https://leetcode.com/problems/"
     
     let url = tabs[0].url;
-    if(url.startsWith(LEETCODE_PROBLEM_URL)) {
+    if (url.startsWith(LEETCODE_PROBLEM_URL)) {
         document.getElementById("readme").disabled = false
     }
 
@@ -94,7 +94,53 @@ document.getElementById("readme").addEventListener("click", function() {
             { code: `(${DOWNLOADER_FUNC})("README.md", ${README_CONTENTS_CODE})` }
         );
     });
-    
 });
 
+document.getElementById("update-counters").addEventListener("click", function() {
+    // Fetch account numbers from Leetcode
+    fetch("https://leetcode.com/api/problems/algorithms/")
+    .then((response) => response.json())
+    .then((data) => chrome.storage.local.get('stats', (data2) => {
+        // Update counters in chrome storage
+        let { stats } = data2;
+        stats.solved = data["num_solved"];
+        stats.easy = data["ac_easy"];
+        stats.medium = data["ac_medium"];
+        stats.hard = data["ac_hard"];
 
+        // Update counters on popup screen
+        document.getElementById("p_solved").innerText = stats.solved;
+        document.getElementById("p_solved_easy").innerText = stats.easy;
+        document.getElementById("p_solved_medium").innerText = stats.medium;
+        document.getElementById("p_solved_hard").innerText = stats.hard;
+    
+        chrome.storage.local.set({ stats }, () => {
+            console.log(
+                `Successfully updated problem counters`,
+            );
+        });
+    }));
+});
+
+document.getElementById("reset-counters").addEventListener("click", function() {
+    chrome.storage.local.get('stats', (data2) => {
+        // Update counters in chrome storage
+        let { stats } = data2;
+        stats.solved = 0;
+        stats.easy = 0;
+        stats.medium = 0;
+        stats.hard = 0;
+        
+        // Update counters on popup screen
+        document.getElementById("p_solved").innerText = stats.solved;
+        document.getElementById("p_solved_easy").innerText = stats.easy;
+        document.getElementById("p_solved_medium").innerText = stats.medium;
+        document.getElementById("p_solved_hard").innerText = stats.hard;
+    
+        chrome.storage.local.set({ stats }, () => {
+            console.log(
+                `Successfully updated problem counters`,
+            );
+        });
+    });
+});
